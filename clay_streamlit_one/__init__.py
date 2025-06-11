4ed5d4f7e24ea180b420cf682effdd619fb907cc
Ariel Elbaz <ariel@arugga.com>
2025-06-11 16:20:25 +0300

    added defaults

--- clay_streamlit_one/__init__.py
+++ clay_streamlit_one/__init__.py
@@ -2,6 +2,6 @@
 __version__ = "2025.06.11"
 
 from .app_page import AppPage
-from .clay_streamlit_app import ClayStreamlitApp
+from .clay_streamlit_app import ClayStreamlitApp, update_defaults_entry
 
 __all__ = ["ClayStreamlitApp", "AppPage"]
